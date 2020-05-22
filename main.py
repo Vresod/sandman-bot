220a78fc9b9dbe46dd9ce50422125adf24847b47
Vresod <lukebinkofsky@gmail.com>
2020-05-21 23:14:56 -0400

    initial commit + boilerplate code

---
+++ main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import time
+import discord
+
+with open("tokenfile", "r") as tokenfile:
+    token=tokenfile.read()
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("logged in as {0.user}".format(client)
+
+@client.event
+async def on_message(message):
+    if message.author.id != 431978032094380043:
+        return
+    if not message.content.startswith("sandman")
+        return
+    args = message.content.replace("sandman","")
+    argslist = args.split(" ")
+    if(argslist[0] == "attack"):
+        message.channel.send("hoogagagagagagagagagaga")
+
+client.run(token)
