f220a9554d73af3005d91161498987a82e20652f
Vresod <lukebinkofsky@gmail.com>
2020-05-21 23:51:20 -0400

    bot now responds upon "sandman attack"

--- main.py
+++ main.py
@@ -4,23 +4,23 @@ import time
 import discord
 
 with open("tokenfile", "r") as tokenfile:
-    token=tokenfile.read()
+	token=tokenfile.read()
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print("logged in as {0.user}".format(client)
+	print("logged in as {0.user}".format(client))
 
 @client.event
 async def on_message(message):
-    if message.author.id != 431978032094380043:
-        return
-    if not message.content.startswith("sandman")
-        return
-    args = message.content.replace("sandman","")
-    argslist = args.split(" ")
-    if(argslist[0] == "attack"):
-        message.channel.send("hoogagagagagagagagagaga")
+	if not (message.author.id == 431978032094380043):
+		return
+	if not message.content.startswith("sandman"):
+		return
+	args = message.content.replace("sandman ","")
+	argslist = args.split(" ")
+	if(argslist[0] == "attack"):
+		await message.channel.send("hoogagagagagagagagagaga")
 
 client.run(token)
