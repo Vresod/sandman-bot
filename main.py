1d1e891a5db9bf5b536cda9dd070e796f137b042
Vresod <lukebinkofsky@gmail.com>
2020-06-22 23:24:35 -0400

    made bot more customizable

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 
 from time import sleep
 import discord
-
+# having a seperate python file for bot settings is probably not the best way to do this
+# but it makes things simpler on the main file. just import the file.
+from settings import *
 with open("tokenfile", "r") as tokenfile:
 	token=tokenfile.read()
 
@@ -22,7 +24,7 @@ async def on_message(message):
 	if message_counter == 25:
 		await message.channel.send("hooga")
 		message_counter = 0
-	if not (message.author.id == 431978032094380043):
+	if not (message.author.id == bot_owner_id): # set bot_owner_id in settings.py
 		return
 	if not message.content.startswith("sandman"):
 		return
@@ -31,10 +33,10 @@ async def on_message(message):
 	if(argslist[0] == "attack"):
 		await message.channel.send("hoogagagagagagagagagaga")
 	if(argslist[0] == "blind"):
-		if(message.mentions[0].id == 431978032094380043 or message.mentions[0].id == 713221599863635978):
+		if(message.mentions[0].id == bot_owner_id or message.mentions[0] == self.user):
 			await message.channel.send("i act in self preservation; i cannot attack my owner or myself")
 			return
-		sandmanrole = message.guild.get_role(713246200555372544) # 713246200555372544 is the sandman role in the server this bot is originally used in
+		sandmanrole = message.guild.get_role(blind_role_id) # set blind_role_id in settings.py
 		await message.channel.send("blinding {0}".format((message.mentions[0]).display_name))
 		await message.mentions[0].add_roles(sandmanrole,reason="blinded by sandman")
 		sleep(10)
